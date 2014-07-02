97f44773f25b3585a1828fb768fbce8252efd149
hongbin8237 <hongbin_yoon@hotmail.com>
2014-07-02 16:27:42 -0500

    initial commit

---
+++ src/merge.cpp
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  merge.cpp
+ *
+ *    Description:  merges reflection and shoreline feature points
+ *
+ *        Version:  1.0
+ *        Created:  07/02/2014 04:26:38 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hong-Bin Yoon (), yoon48@illinois.edu
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+{
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
