47c5c4c772b72713534f4d9191301e3690075a77
hongbin8237 <hongbin_yoon@hotmail.com>
2014-07-03 12:21:53 -0500

    .

--- src/merge.cpp
+++ src/merge.cpp
@@ -1,3 +1,14 @@
+#include    <math.h>
+#include    <iostream>
+#include    <stdlib.h>
+#include    <cstdlib>
+#include    <cstdio>
+#include    "Quaternion.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#define MAXLINE 512 
+
 /*
  * =====================================================================================
  *
@@ -28,6 +39,71 @@
  */
     int
 main ( int argc, char *argv[] )
-{
+{  
+    line	= (char *) calloc ( (size_t)(MAXLINE), sizeof(char) );
+    if ( line==NULL ) {
+        fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+        exit (EXIT_FAILURE);
+    }
+
+    /*  Total Number of Features */
+    int N = 5;
+    int n = 0;
+    bool refl; // fills with reflection data when true
+    FILE* fp_r = fopen("REFLECTION_PATH" , 'r'); // TODO: EDIT PATHS
+    FILE* fp_s = fopen("SHORELINE_PATH", 'r');   
+    while(1)
+    {
+        refl = true;
+        while(n<N)
+        {
+            if(refl) // fill with reflection
+            {
+                if( (fgets(line,MAXLINE,fp_r ))==NULL )
+                    err_sys("fgets reflection pib");
+                
+                if( line[0] == '\n' )
+                {
+                    refl = false;
+                    continue;
+                }
+                else
+                {
+                    printf("%s",line);
+                    n++;
+                }
+            }
+            else  // fill with shoreline
+            {
+
+
+                if( (fgets(line,MAXLINE,fp_s ))==NULL )
+                    err_sys("fgets reflection pib");
+
+                if( line[0] == '\n' )
+                {
+
+                    continue;
+                }
+                else
+                {
+                    printf("%s",line);
+                    n++;
+                }
+            }
+        }
+
+        while((fgets(line,MAXLINE,fp_s ))!=NULL )  // reads rest of the shoreline
+        {
+            if( line[0] == '\n' )
+            {
+                printf("%s",line);
+                continue;
+            }
+        }
+
+    }
+    free (line);
+    line	= NULL;
     return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
